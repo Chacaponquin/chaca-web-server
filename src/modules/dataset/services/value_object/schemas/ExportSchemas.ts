d3a20db5fa2d692032fad8d0ecc73503f46441d5
Héctor Angel Gomez <hectorangel2001@gmail.com>
2024-01-29 17:39:14 -0500

    Fix public route

--- src/modules/dataset/services/value_object/schemas/ExportSchemas.ts
+++ src/modules/dataset/services/value_object/schemas/ExportSchemas.ts
@@ -9,7 +9,7 @@ interface Props {
 }
 
 export class ExportSchemas extends Generator {
-  private readonly ROUTE = "/modules/dataset/infrastructure/s3/core";
+  private readonly ROUTE = "dist/src/modules/dataset/infrastructure/s3/core";
 
   constructor(private readonly schemas: Array<MultiGenerateSchema>) {
     super();
